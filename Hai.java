f4177c6561e78bff8d03ddb39e00412004cda13e
n-azizah <azizahn346@gmail.com>
2020-03-19 03:34:59 +0800

    Hai Teman & Dunia

--- Hai.java
+++ Hai.java
@@ -1,5 +1,6 @@
 public class Hai{
  public static void main (String [] args){
-  System.out.println("Hai Semua !!");
+  System.out.println("Hai Teman !!");
+  System.out.println("Hai Dunia !!");
  }
 }
